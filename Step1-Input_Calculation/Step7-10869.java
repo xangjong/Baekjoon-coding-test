8a38c39329aa57fc9b49464648b1c73d49b974ae
xangjong <csj7080@naver.com>
2022-10-14 14:20:32 +0900

    Add 10869번 문제

---
+++ Step1-Input_Calculation/Step7-10869.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] ags){
+        Scanner sc = new Scanner(System.in);
+       
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
