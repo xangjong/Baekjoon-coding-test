19df2c76ea3f8489fa0c7c85804826aaf6d3a65c
xangjong <csj7080@naver.com>
2022-10-25 12:28:55 +0900

    Add 11021

---
+++ Step3-for/Step06-11021.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+
+		for (int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			System.out.println("Case #" + i + ": " + (A + B));
+		}
+		br.close();
+
+	}
+}
