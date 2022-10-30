96e401926adec2bae98cea83167955e216f1fc04
xangjong <csj7080@naver.com>
2022-10-30 18:17:17 +0900

    Add 1110

---
+++ Step3-for/Step10-1110.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int copy = N;
+		int count = 0;
+
+		do {
+			N = (((N % 10) * 10) + ((N / 10) + (N % 10)) % 10);
+			count++;
+		} while (N != copy);
+
+		System.out.println(count);
+
+	}
+
+}
