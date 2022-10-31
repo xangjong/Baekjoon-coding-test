6d06949eef4b9e5c774587c5e821915071f3e4c8
xangjong <csj7080@naver.com>
2022-10-31 15:52:24 +0900

    Add 10952

---
+++ Step3-for/Step11-10952.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		while (true) {
+
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			if (A + B == 0) {
+				break;
+			}
+			System.out.println(A + B);
+		}
+		br.close();
+	}
+}
