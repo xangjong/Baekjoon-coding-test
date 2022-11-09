fba0e8539ffc3006fefb0f3f5587ccc1147da44b
xangjong <csj7080@naver.com>
2022-11-10 01:40:30 +0900

    Rename : README 추가

---
+++ Step2-If/14681.사분면 고르기/14681.사분면 고르기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int X = Integer.parseInt(br.readLine());
+		int Y = Integer.parseInt(br.readLine());
+
+		if (X > 0) {
+			if (Y > 0) {
+				System.out.println(1);
+			} else {
+				System.out.println(4);
+			}
+		} else {
+			if (Y > 0) {
+				System.out.println(2);
+			} else {
+				System.out.println(3);
+			}
+		}
+
+	}
+}
