601176e9664599f2ed506f6a104d8275c1bcfe98
xangjong <101630615+xangjong@users.noreply.github.com>
2022-11-21 20:54:41 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int grade = Integer.parseInt(st.nextToken());
+        
+        br.close();
+        
+        if(grade>=90 && grade <=100) {
+            System.out.println("A");
+        } else if(grade>=80) {
+            System.out.println("B");
+        } else if(grade>=70) {
+            System.out.println("C");
+        } else if(grade>=60) {
+            System.out.println("D");
+        } else {
+             System.out.println("F");
+        }
+        
+    }
+}
