448b9b570e76671a83e6f5532b097d989e7d6acd
xangjong <101630615+xangjong@users.noreply.github.com>
2022-11-22 19:15:29 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+           	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        int year = Integer.parseInt(st.nextToken());
+        
+        br.close();
+        
+        if( (year % 4 == 0)){
+            if( (year % 100 != 0) ||  (year % 400 == 0))  {
+            	System.out.println(1);
+            }  else {
+           	 	System.out.println(0);
+            }
+        } else {
+        	 System.out.println(0);
+        }
+        
+    }
+}
