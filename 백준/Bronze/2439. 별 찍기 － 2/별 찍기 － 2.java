60c8a09acf713c947a4627c3c95bd23af9cf4526
xangjong <101630615+xangjong@users.noreply.github.com>
2022-12-02 13:30:53 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				sb.append(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		System.out.print(sb);
+	}
+}
